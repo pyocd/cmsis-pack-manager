a720f1e9256ce88e3ddc86983c15839c4bf88aff
Jimmy Brisson <theotherjimmy@gmail.com>
2017-03-21 16:46:38 -0500

    Add Mid-level unit tests

--- tests/unit.py
+++ tests/unit.py
@@ -2,9 +2,11 @@
 
 from os.path import join
 from string import ascii_lowercase
-from mock import patch, MagicMock
+from mock import patch, MagicMock, call
 from hypothesis import given, settings, example
-from hypothesis.strategies import booleans, text, lists, just, integers
+from hypothesis.strategies import booleans, text, lists, just, integers, tuples
+from jinja2 import Template
+from bs4 import BeautifulSoup
 
 import ArmPackManager
 
@@ -42,8 +44,9 @@ def test_strip_protocol(protocol, url):
 def test_cache_file(url, contents):
     @patch("ArmPackManager.Cache.display_counter")
     @patch("ArmPackManager.urlopen")
+    @patch("ArmPackManager.makedirs")
     @patch("ArmPackManager.open", create=True)
-    def inner_test(_open, _urlopen, _):
+    def inner_test(_open, _, _urlopen, __):
         _open.return_value = MagicMock(spec=file)
         _urlopen.return_value.read.return_value = contents
         c = ArmPackManager.Cache(True, True)
@@ -76,4 +79,65 @@ def test_pack_from_cache(data_path, url):
         device = {'pack_file': url}
         c.pack_from_cache(device)
         _zf.called_with(join(data_path, url))
+    inner_test()
+
+VERSION_TEMPLATE = (
+    "<package>"
+    "<vendor>{{vendor}}</vendor>"
+    "<name>{{name}}</name>"
+    "<url>{{url}}</url>"
+    "<releases>"
+    "{% for version in versions %}"
+    "<release version=\"{{version}}\"></release>"
+    "{% endfor %}"
+    "</releases>"
+    "</package>")
+
+@given(text(alphabet=ascii_lowercase), text(alphabet=ascii_lowercase),
+       text(alphabet=ascii_lowercase + ":/_."),
+       lists(text(min_size=1), min_size=1))
+def test_pdsc_to_pack(name, vendor, url, versions):
+    xml = Template(VERSION_TEMPLATE).render(name=name, vendor=vendor, url=url,
+                                            versions=versions)
+    @patch("ArmPackManager.largest_version")
+    @patch("ArmPackManager.Cache.pdsc_from_cache")
+    def inner_test(pdsc_from_cache, largest_version):
+        pdsc_from_cache.return_value = BeautifulSoup(xml, "html.parser")
+        largest_version.return_value = versions[0]
+        c = ArmPackManager.Cache(True, True)
+        new_url = c.pdsc_to_pack(url + "desc.pdsc")
+        assert new_url.startswith(url)
+        assert new_url.endswith("%s.%s.%s.pack" % (vendor, name, versions[0]))
+    inner_test()
+
+@given(text(alphabet=ascii_lowercase + ":/_."),
+       text(alphabet=ascii_lowercase + ":/_."))
+def test_cache_pdsc_and_pack(pack_url, pdsc_url):
+    @patch("ArmPackManager.Cache.cache_file")
+    @patch("ArmPackManager.Cache.pdsc_to_pack")
+    def inner_test(pdsc_to_pack, cache_file):
+        pdsc_to_pack.return_value = pack_url
+        c = ArmPackManager.Cache(True, True)
+        c.cache_pdsc_and_pack(pdsc_url)
+        cache_file.assert_has_calls([call(pdsc_url), call(pack_url)])
+    inner_test()
+
+IDX_TEMPLATE = (
+    "{% for name, url in pdscs %}"
+    "<pdsc name=\"{{name}}\" url=\"{{url}}\"/>"
+    "{% endfor %}")
+
+@given(lists(tuples(text(alphabet=ascii_lowercase, min_size=1),
+                    text(alphabet=ascii_lowercase + ":/_.", min_size=1)),
+             min_size=1))
+def test_get_urls(pdscs):
+    xml = Template(IDX_TEMPLATE).render(pdscs=pdscs)
+    @patch("ArmPackManager.Cache.pdsc_from_cache")
+    def inner_test(pdsc_from_cache):
+        pdsc_from_cache.return_value = BeautifulSoup(xml, "html.parser")
+        c = ArmPackManager.Cache(True, True)
+        urls = c.get_urls()
+        for url in urls:
+            assert any((url.startswith(pdsc[1].rstrip("/")) and
+                        url.endswith(pdsc[0].strip("/"))) for pdsc in pdscs)
     inner_test()
