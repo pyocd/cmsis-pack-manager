b9de1bfc9d4ad49582463bee99df60acb5305f0f
Jimmy Brisson <theotherjimmy@gmail.com>
2017-04-05 13:15:24 -0500

    Rename to cmsis-pack-maneger

--- cmsis_pack_manager/pack_manager.py
+++ cmsis_pack_manager/pack_manager.py
@@ -18,7 +18,7 @@ from os.path import basename, join, dirname, exists
 from os import makedirs
 from itertools import takewhile
 from fuzzywuzzy import process
-from ArmPackManager import Cache
+from cmsis_pack_manager import Cache
 
 parser = argparse.ArgumentParser(description='A Handy little utility for keeping your cache of pack files up to date.')
 subparsers = parser.add_subparsers(title="Commands")
