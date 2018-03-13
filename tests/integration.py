a8e360c6250af0f02e9475a0023a4b159e0aae83
Jimmy Brisson <theotherjimmy@gmail.com>
2018-03-13 11:27:03 -0500

    Test dump-parts CLI

--- tests/integration.py
+++ tests/integration.py
@@ -10,7 +10,7 @@ import sys
 import threading
 import urllib
 import tempfile
-from os.path import join, dirname
+from os.path import join, dirname, exists
 
 import cmsis_pack_manager
 import cmsis_pack_manager.pack_manager
@@ -54,11 +54,39 @@ def test_pull_pdscs_cli():
                 "--json-path", json_path,
                 "--vidx-list", join(dirname(__file__), 'test-pack-index', 'vendors.list')]
     cmsis_pack_manager.pack_manager.main()
+    httpd.shutdown()
     c = cmsis_pack_manager.Cache(True, True, json_path=json_path, data_path=data_path)
     assert("MyDevice" in c.index)
     assert("MyBoard" in c.aliases)
     assert("MyDevice" in c.aliases["MyBoard"]["mounted_devices"])
     assert(c.pack_from_cache(c.index["MyDevice"]).open("MyVendor.MyPack.pdsc"))
+
+def test_dump_parts_cli():
+    socketserver.TCPServer.allow_reuse_address = True
+    PORT = 8001
+    handler = http.SimpleHTTPRequestHandler
+    httpd = socketserver.TCPServer(("", PORT), handler)
+    httpd_thread = threading.Thread(target=httpd.serve_forever)
+    httpd_thread.setDaemon(True)
+    httpd_thread.start()
+
+    json_path = tempfile.mkdtemp()
+    data_path = tempfile.mkdtemp()
+    sys.argv = ["pack-manager", "cache", "-e", "--data-path", data_path,
+                "--json-path", json_path,
+                "--vidx-list", join(dirname(__file__), 'test-pack-index', 'vendors.list')]
+    cmsis_pack_manager.pack_manager.main()
+    httpd.shutdown()
+    dump_path = tempfile.mkdtemp()
+    sys.argv = ["pack-manager", "dump-parts", dump_path, "Dev",
+                "--data-path", data_path,
+                "--json-path", json_path]
+    cmsis_pack_manager.pack_manager.main()
+    c = cmsis_pack_manager.Cache(True, True, json_path=json_path, data_path=data_path)
+    assert exists(join(dump_path, "index.json"))
+    for algo in c.index["MyDevice"]["algorithms"]:
+        assert exists(join(dump_path, algo["file_name"]))
+
 
 def test_panic_handling():
     from cmsis_pack_manager import ffi
