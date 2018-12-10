909052fe1c3035c07e6c589bbff9d1105a1f18c8
Jimmy Brisson <theotherjimmy@gmail.com>
2018-12-10 10:37:47 -0600

    Fix nightly install WRT llv-tools-preview
    see https://github.com/rust-lang/rust/issues/55866 for details

--- build-wheels.sh
+++ build-wheels.sh
@@ -4,7 +4,7 @@ set -e -x
 function install_rust {
     curl https://static.rust-lang.org/rustup.sh > /tmp/rustup.sh
     chmod +x /tmp/rustup.sh
-    /tmp/rustup.sh -y --disable-sudo --channel=$1
+    /tmp/rustup.sh -y --disable-sudo --channel=$1 --without=llvm-tools-preview
 }
 
 function clean_project {
