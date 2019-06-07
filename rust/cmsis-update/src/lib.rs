470950a1a92adff5edd24700e59d751922f7a698
Jimmy Brisson <theotherjimmy@gmail.com>
2019-06-07 11:10:06 -0500

    Simplify download code by packaging context into a struct

--- rust/cmsis-update/src/lib.rs
+++ rust/cmsis-update/src/lib.rs
@@ -12,46 +12,23 @@ extern crate utils;
 extern crate pack_index;
 extern crate pdsc;
 
-use hyper::{Body, Client};
-use hyper::client::Connect;
+use hyper::Client;
 use hyper_rustls::HttpsConnector;
 use tokio_core::reactor::Core;
 use std::path::PathBuf;
 use slog::Logger;
 use failure::Error;
+use futures::stream::iter_ok;
+use futures::Stream;
 
 use pdsc::Package;
 
 mod redirect;
 mod vidx;
 mod download;
-mod dl_pdsc;
-mod dl_pack;
 
-use dl_pdsc::{update_future};
-use dl_pack::{install_future};
-pub use download::DownloadProgress;
-pub use download::DownloadConfig;
-
-
-// This will "trick" the borrow checker into thinking that the lifetimes for
-// client and core are at least as big as the lifetime for pdscs, which they actually are
-fn update_inner<'a, C, I, P, D>(
-    config: &'a D,
-    vidx_list: I,
-    core: &'a mut Core,
-    client: &'a Client<C, Body>,
-    logger: &'a Logger,
-    progress: P,
-) -> Result<Vec<PathBuf>, Error>
-where
-    C: Connect,
-    I: IntoIterator<Item = String>,
-    P: DownloadProgress + 'a,
-    D: DownloadConfig,
-{
-    core.run(update_future(config, vidx_list, client, logger, progress))
-}
+use vidx::{download_vidx_list, flatmap_pdscs};
+pub use download::{DownloadConfig, DownloadContext, DownloadProgress};
 
 /// Flatten a list of Vidx Urls into a list of updated CMSIS packs
 pub fn update<I, P, D>(config: &D, vidx_list: I, logger: &Logger, progress: P) -> Result<Vec<PathBuf>, Error>
@@ -66,26 +43,15 @@ where
         .keep_alive(true)
         .connector(HttpsConnector::new(4, &handle))
         .build(&handle);
-    update_inner(config, vidx_list, &mut core, &client, logger, progress)
-}
-
-// This will "trick" the borrow checker into thinking that the lifetimes for
-// client and core are at least as big as the lifetime for pdscs, which they actually are
-fn install_inner<'client, 'a: 'client, C, I: 'a, P: 'client, D>(
-    config: &'a D,
-    pdsc_list: I,
-    core: &mut Core,
-    client: &'client Client<C, Body>,
-    logger: &'a Logger,
-    progress: P
-) -> Result<Vec<PathBuf>, Error>
-    where
-    C: Connect,
-    I: IntoIterator<Item = &'a Package>,
-    P: DownloadProgress + 'a,
-    D: DownloadConfig,
-{
-    core.run(install_future(config, pdsc_list, client, logger, progress))
+    let dl_cntx = DownloadContext::new(config, progress, &client, logger);
+    let fut = {
+        let parsed_vidx = download_vidx_list(vidx_list, &client, logger);
+        let pdsc_list = parsed_vidx
+            .filter_map(|vidx| vidx.map(|v| flatmap_pdscs(v, &client, logger)))
+            .flatten();
+        dl_cntx.download_stream(pdsc_list).collect()
+    };
+    core.run(fut)
 }
 
 /// Flatten a list of Vidx Urls into a list of updated CMSIS packs
@@ -106,5 +72,9 @@ pub fn install<'a, I: 'a, P, D>(
         .keep_alive(true)
         .connector(HttpsConnector::new(4, &handle))
         .build(&handle);
-    install_inner(config, pdsc_list, &mut core, &client, logger, progress)
+    let dl_cntx = DownloadContext::new(config, progress, &client, logger);
+    let fut = {
+        dl_cntx.download_stream(iter_ok(pdsc_list)).collect()
+    };
+    core.run(fut)
 }
